3ae0be20b6dfa8a044e09958ddabb837889e9dab
Steven L. Speek <slspeek@gmail.com>
2014-01-01 21:26:53 +0100

    -no-ssl and -db flags

--- main/app.go
+++ main/app.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/slspeek/go-restful"
 	"github.com/slspeek/gotube/auth"
@@ -9,6 +11,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"syscall"
 )
 
 var (
@@ -21,17 +24,27 @@ func check(err error) {
 	}
 }
 
+var noSsl = flag.Bool("no-ssl", false, "run without SSL")
+var dbName = flag.String("db", "gotube", "name of the mongodb to use")
+
 func main() {
+	flag.Parse()
 	var err error
 	sess, err = mgo.Dial("localhost")
 	if err != nil {
 		panic(err)
 	}
+	f, err := os.Create("gotube.pid")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(f, syscall.Getpid())
+  f.Close()
 
 	r := mux.NewRouter()
 
 	authenticator := auth.Authenticator("htpasswd")
-	videoService := rest.NewVideoResource(sess.Copy(), "gotube", "Video", &auth.Auth{authenticator.CheckAuth})
+	videoService := rest.NewVideoResource(sess.Copy(), *dbName, "Video", &auth.Auth{authenticator.CheckAuth})
 	container := restful.NewContainer()
 	videoService.Register(container)
 	container.Filter(container.OPTIONSFilter)
@@ -40,7 +53,10 @@ func main() {
 
 	r.PathPrefix("/api/videos").Handler(container)
 	r.PathPrefix("/content/videos").Handler(container)
-	r.PathPrefix("/").Handler(http.FileServer(http.Dir(os.Args[1])))
-	//log.Fatal(http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", r))
-	log.Fatal(http.ListenAndServe(":8080", r))
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir(flag.Arg(0))))
+	if !*noSsl {
+		log.Fatal(http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", r))
+	} else {
+		log.Fatal(http.ListenAndServe(":8080", r))
+	}
 }
