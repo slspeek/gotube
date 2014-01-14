cffaa78c5045df949b15c4728c2f0fd380f211a4
Steven L. Speek <slspeek@gmail.com>
2014-01-15 00:42:23 +0100

    navbar active link

--- web/app/scripts/services/Page.js
+++ web/app/scripts/services/Page.js
@@ -1,14 +1,36 @@
 'use strict';
 
 angular.module('webApp').
-factory('Page', function() {
+factory('Page', function($rootScope, $location) {
   var title = 'Gotube';
+  var onUpload = false;
+  var onMyVideos = false;
+  $rootScope.$on('$routeChangeSuccess', function(ev, current, previous) {
+    var current =  $location.path();
+    console.log('current: ' + current);
+    if (current === '/upload') {
+      onUpload = true;
+    } else {
+      onUpload = false;
+    }
+    if (current === '/list') {
+      onMyVideos = true;
+    } else {
+      onMyVideos = false;
+    }
+  });
   return {
     title: function() {
       return title;
     },
     setTitle: function(newTitle) {
       title = newTitle;
+    },
+    onUpload: function() {
+      return onUpload;
+    },
+    onMyVideos: function() {
+      return onMyVideos;
     }
   };
 });
--- web/test/spec/services/Page.js
+++ web/test/spec/services/Page.js
@@ -6,10 +6,12 @@ describe('Service: Page', function() {
   beforeEach(module('webApp'));
 
   // instantiate service
-  var Page;
-  beforeEach(inject(function(_Page_) {
-    Page = _Page_;
-  }));
+  var Page, $rootScope, $location;
+  beforeEach(inject(['Page', '$rootScope', '$location', function(P, $rS, $l) {
+    Page = P;
+    $rootScope = $rS;
+    $location = $l;
+  }]));
 
   it('should default to Gotube', function() {
     expect(Page.title()).toBe('Gotube');
@@ -20,5 +22,26 @@ describe('Service: Page', function() {
     Page.setTitle(tt);
     expect(Page.title()).toBe(tt);
   });
+  
+  it('should set onUpload to false', function() {
+    expect(Page.onUpload()).toBe(false);
+    
+  });
 
+  it('should set onMyVideos to false', function() {
+    expect(Page.onMyVideos()).toBe(false);
+  });
+  
+  it('should set onMyVideos with /list route', function() {
+    $location.path('/list');
+    $rootScope.$broadcast('$routeChangeSuccess', '', '/list');
+    expect(Page.onMyVideos()).toBe(true);
+    });
+  
+  
+  it('should set onUpload with /upload route', function() {
+    $location.path('/upload');
+    $rootScope.$broadcast('$routeChangeSuccess', '', '/list');
+    expect(Page.onUpload()).toBe(true);
+    });
 });
